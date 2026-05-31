caa4218d529627124ecb96a569c2d208a644d8c9
Theodore Grist <theodoreg23456@gmail.com>
2026-05-31 05:53:54 +0200

    Minor changes to prefab and gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,21 @@
+using cherrydev;
+using NUnit.Framework;
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class GameManager : MonoBehaviour
 {
+    public DialogNodeGraph[] AntonyNodes;
+    public DialogNodeGraph[] SamanthaNodes;
+    public DialogNodeGraph[] JamesNodes;
+    public DialogNodeGraph[] PhoebeNodes;
     public static GameManager instance;
     
     [NonSerialized]
     public int nextScene = 1;
+    
     
     void Start()
     {
